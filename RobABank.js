555e0cb92b1f0b90e48420e8148aa08afa24e1e3
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-03-20 12:06:14 -0600

    Create RobABank.js

---
+++ RobABank.js
@@ -0,0 +1,11 @@
+// 1. I would decide on what bank to rob, there can be multiple banks, but finding the most adequate one will be hard based on location and the sorrounding terrain
+// 2. I would have to decided which one is best I need to find out how long will law enforcement will get there and act, and how many civilians will notice that there is something wrong
+// 3. After finding the best place that will meet all requirements, I will have to research about that specific bank, the people that work there, and the most common customers
+// 4. Before anymore steps I will need people, people that are willing to commit the crime and risk their lives for fortune. I need people that will not snitch or have fear
+// 5. This will take years of planning and preperations, forming the right friends for the job, and getting into buying and selling drugs
+// 6. Why drugs? I will need to get close to people that already have commited crimes and are willing to do this, hangout out with them, get to know them as people
+// 7. Finding the right people will be hard, but I will also need people who have attributes, like skills.
+// 8. After finding the right people and getting to know them then we go to the planning stage and more research
+// 9. I will have to intice them that money is worth their lives, I will need to build charisma to not only make them more confident, but make them willing to do it
+// 10. I will also need information inside the bank I will need to research the individuals, what they have in their social media, where they hangout.
+// 11. Having a person who will provide information will be beneficial, if they work for the bank I can inticite them to provide information
